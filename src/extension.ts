54ea4fc0205ef6c5eda73f3ab00997099f64067d
C. Thomas Brittain <honeysucklehardware@gmail.com>
2020-08-10 22:33:01 -0500

    Working

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const SerialPort = require('serialport');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	var port_paths: String[] = [''];
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "micro-python-terminal" is now active!');
+
+	readAvailablePorts().then((paths) => {
+		port_paths = paths;
+		console.log(port_paths);
+	}).catch((err) => {
+		port_paths = err;
+	});
+	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -21,6 +32,21 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function readAvailablePorts()  {
+	return new Promise<String[]>((resolve, reject) => {
+		var paths: String[];
+		SerialPort.list().then((ports: [any]) => {
+			ports.forEach(port => {
+				paths.push(port.path);
+			});
+			resolve(paths);
+		}).catch((err: any) => {
+			console.log(err);
+			reject(['No devices found.']);
+		});;
+	});
 }
 
 // this method is called when your extension is deactivated
