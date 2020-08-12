7db0b7e6871348c4c9c3ec903d72e57e9b943f73
ladvien <honeysucklehardware@gmail.com>
2020-08-11 23:17:11 -0500

    Working on sendText

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,23 @@
 import * as vscode from 'vscode';
 import { window } from 'vscode';
+import { Position, Range } from 'vscode';
+
+import { selectPort, selectBaud } from './serialPort';
 
 import { checkIfMicroPyTermExists, sendTextToTerm, ensureTerminalExists, 
 	     selectTerminal, selectMicroPythonTerm } from './pyTerminal';
-import { selectPort, selectBaud } from './serialPort';
+
+
+
 import { delay } from './util';
 import { REPL } from './repl';
-
 const SerialPort = require('serialport');
 
+
+const SEND_THROTTLE = 100;
+
 let repl: REPL;
+
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -17,7 +25,31 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('micro-python-terminal.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from µ-python-terminal!');
+		
 	});
+
+	let sendTextTermCommand = vscode.commands.registerCommand('micro-python-terminal.sendTextTermCommand', async () => {
+		const terminal = selectMicroPythonTerm(vscode.window.terminals);
+		if (undefined !== terminal){
+			if (undefined !== window.activeTextEditor?.document) {
+				const doc = window.activeTextEditor?.document;
+				const { start, end } = window.activeTextEditor.selection;
+				const textRange = new Range(start, end);
+
+				let lines = doc.getText(textRange).split('\n'); 
+
+				for (let i = 0; i < lines.length; i++) {
+					const line = lines[i];
+					line !== '\n' ? terminal.sendText(line.trim()) : true ;
+					console.log(line);
+					await delay(SEND_THROTTLE);
+				}
+			}
+		} else {
+			window.showErrorMessage('No open document.')
+		}
+	});
+
 
 	let microPyTerm = vscode.commands.registerCommand('micro-python-terminal.createTerm', () => {
 		const terminal = selectMicroPythonTerm(vscode.window.terminals);
@@ -35,8 +67,9 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(microPyTerm);
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(microPyTerm);
+	context.subscriptions.push(sendTextTermCommand)
 }
 
 export function deactivate() {
